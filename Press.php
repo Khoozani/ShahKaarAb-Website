72964eac31f0caffaf606b9024fdd3694473bd11
Khoozani <ShahKhoozani@Gmail.com>
2026-08-29 17:42:47 +0330

    Add files via upload

---
+++ Press.php
@@ -0,0 +1,724 @@
+<!doctype html>
+
+<html>
+
+	<head>
+   <meta name="robots" content="index, follow">
+    <meta charset="UTF-8">
+    <meta charset="UTF-8">
+    <meta name="description" content="Shah - ab Khoozani Personal Website">
+    <meta name="keywords" content="Shahab Khoozani
+                                 ,شهاب خوزانی
+                                 ,Shahaboddin Habibi Khoozani
+                                 ,شهاب الدین حبیبی خوزانی
+                                 ,Personal Website
+                                 ,وبسایت شخصی
+                                 ,Shaha - ab 
+                                 ,شاه آب
+                                 ,Made In Persian Gulf
+                                 ,ساخت شاخآب پارس
+                                 ,صنع فی الخلیج الفارسی
+                                 ,Website Development
+                                 ,Game Development
+                                 ,App Development
+                                 ,Motion Graphic
+                                 ,Animation
+                                 ,   
+                                   ">
+  <meta name="author" content="Shahab Khoozani
+                               ,شهاب خوزانی
+                               ,Shahaboddin Habibi Khoozani
+                               ,شهاب الدین حبیبی خوزانی
+                               ,Shaha - ab
+                               ,شاه آب">
+	<meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi" />
+    <title>Dorood درود</title>
+
+	</head>
+
+	 
+	<style type="text/css"> 
+		  @media (orientation: Portrait) {
+			 			 
+                 #warning-message { 
+					 display:block;
+					 
+					 left: 0%;
+					 top: 0%
+						 
+						 
+			  
+			  }
+				 #home {	 
+			 display:none;
+		
+		 }	
+			  }
+		 
+		 @media (orientation: landscape) {
+			   #warning-message { display:none; }
+			 	 #home {	 
+			 visibility:visible;
+		 }	
+		   
+     	
+		 #body {
+	         background-color:wheat;
+	     }
+		 
+		
+		 #home {
+			 visibility:visible;  
+		 }	 
+		 
+		 #Customers_BG { 
+			 position:absolute;
+			 left: 10%;
+             top: 10%;  
+		 }
+		 
+		 #Box {
+			 position:absolute;
+			 left: 2%;
+             top: 0%;
+			 width: 30%:
+		 }
+		 
+	   	 #Top1 {
+			 position:absolute;
+			 left: 43.5%;
+			 width: 25%;
+             top: 11%;
+		 }
+		
+     	   #Top2 {
+			 position:absolute;
+			 left: 43.5%;
+             top: 12%;
+			   
+		     height: 87.2%;
+		 }
+		   #Top3 {
+			 position:absolute;
+			 left: 84.3%;
+             top: 12%;
+		     height: 87.2%;
+		 }
+		   #Top4 {
+		     position:absolute;
+			 left: 43.5%;
+			   width: 25%;
+             top: 50%;
+		     
+		 }
+		   #Top5 {
+			position:absolute;
+			 left: 43.5%;
+			   width: 25%;
+             top: 93.4%;
+		 }
+		  
+		  #Sample {
+			position:absolute;
+			 left: 43.5%;
+             top: 93.4%;
+			  
+		 }
+		   #Sample_G_0 {
+			position:absolute;
+			 left: 45%;
+             top: 15%;
+			 width: 40%;
+			  
+		 }
+		  #Sample_G_1 {
+			position:absolute;
+			 left: 45%;
+             top: 15%;
+			 width: 40%;
+			  
+		 }
+		  #Sample_G_2 {
+			 position:absolute;
+			 left: 43%;
+             top: 20%;
+			 width: 60%;
+			  
+		 }
+		   #Sample_G_3 {
+			 position:absolute;
+			 left: 44.9%;
+             top: 14%;
+			 width: 40%;
+			  
+		 }
+		 
+		  #Sample_W_0 {
+			 position:absolute;
+			 left: 50%;
+             top: 12%;
+			 width: 28%;
+			  
+			  
+		 }
+		  #Sample_W_1 {
+			 position:absolute;
+			 left: 53%;
+             top: 13%;
+			 width: 20%;
+			  
+		 }
+		  #Sample_W_2 {
+			position:absolute;
+			 left: 45%;
+             top: 19%;
+			 width: 40%;
+			  
+			  
+			  
+		 }
+		   #Sample_W_3 {
+			position:absolute;
+			 left: 45%;
+             top: 20%;
+			 width: 40%;
+			  
+			  
+		 }
+		  #Sample_A_0 {
+			 position:absolute;
+			 left: 54%;
+             top: 12%;
+			 width: 20%;
+			  
+			  
+		 }
+		  #Sample_A_1 {
+			 position:absolute;
+			 left: 54%;
+             top: 12%;
+			 width: 20%;
+			  
+			  
+		 }
+		  #Sample_A_2 {
+			 position:absolute;
+			 left: 45%;
+             top: 19%;
+			 width: 40%;
+			  
+		 }
+		   #Sample_A_3 {
+			 position:absolute;
+			 left: 45%;
+             top: 19%;
+			 width: 40%;
+			  
+			  
+		 }
+		  #Sample_Mo_0 {
+			 position:absolute;
+			 left: 47%;
+             top: 20%;
+			 width: 35%; 
+		 }
+			 
+		  #Sample_Mo_1 {
+			 position:absolute;
+			 left: 47%;
+             top: 20%;
+			 width: 35%;
+		 }
+			 
+		  #Sample_Mo_2 {
+			position:absolute;
+			 left: 47%;
+             top: 20%;
+			 width: 35%; 
+		 }
+			 
+		   #Sample_Mo_3 {
+             position:absolute;
+			 left: 47%;
+             top: 20%;
+			 width: 35%;  
+		 }
+		  #Sample_A2_0 {
+			 position:absolute;
+			 left: 34%;
+             top: 18%;
+			 width: 60%;
+		     height: 40%;
+			  
+			  
+		 }
+		  #Sample_A2_1 {
+			position:absolute;
+			 left: 34%;
+             top: 18%;
+			 width: 60%;
+		     height: 40%;
+			  
+			  
+		 }
+		  #Sample_A2_2 {
+			 position:absolute;
+			 left: 34%;
+             top: 18%;
+			 width: 60%;
+		     height: 40%;
+			  
+			  
+			  
+		 }
+		   #Sample_A2_3 {
+			 position:absolute;
+			 left: 34%;
+             top: 18%;
+			 width: 60%;
+		     height: 40%;
+			  
+			  
+			  
+		 }
+		  #Sample_MS_0 {
+			 position:absolute;
+			 left: 36%;
+             top: 14%;
+			  
+			  
+		 }
+		  #Sample_MS_1 {
+		     position:absolute;
+			 left: 36%;
+             top: 14%;
+			  
+			  
+		 }
+		  #Sample_MS_2 {
+			 position:absolute;
+			 left: 36%;
+             top: 14%;
+			  
+			  
+		 }
+		   #Sample_MS_3 {
+			 position:absolute;
+			 left: 36%;
+             top: 14%;
+			  
+			  
+		 }
+		  
+			 #Sample_L_0  {
+		   	 position:absolute;
+			 left: 45%;
+             top: 20%;
+			 width: 40%; 
+			  
+		    	}
+		 
+		
+			 #Sample_L_1 {
+			 position:absolute;
+			 left: 45%;
+             top: 20%;
+			 width: 40%; 
+			  
+		        }
+		
+			 #Sample_L_2 {
+			 position:absolute;
+			 left: 45%;
+             top: 20%;
+			 width: 40%; 
+		        }
+			 
+		     #Sample_L_3 {
+			 position:absolute;
+			 left: 45.2%;
+             top: 20%;
+			 width: 39.5%; 
+		        }
+
+	         #SampleD {
+			 position:absolute;
+			 left: 45%;
+             top: 58%;
+		     width: 40%;
+			   
+			 }
+
+		
+			 #SampleD_L1 {
+			 position:absolute;
+			 left: 50%;
+             top: 56%;
+		     width: 32%;
+			 height: 38%;
+			   
+			 }
+
+		 
+		     #Game {
+			 position:absolute;
+			 left: 5%;
+             top: 35%;
+			 width: 25%;
+			 cursor: pointer;
+		    	}
+			 #WebD {
+			 position:absolute;
+			 left: 5%;
+             top: 42%;
+		     width: 25%;
+			 cursor: pointer;
+        	   }
+			 #AppD {
+			 position:absolute;
+			 left: 5%;
+             top: 49%;
+			 width: 25%;
+			 cursor: pointer;
+		    	}
+		
+		
+	
+		 
+		 	 #Back {
+			 position:absolute;
+			 left: 5%;
+             top: 89%;
+			 width: 25%;
+			 cursor: pointer;
+		    	}
+		 
+			 #Left {
+			 position:absolute;
+			 left: 60%;
+             top: 2.5%;
+			 width: 4%;
+			 cursor: pointer;
+		    	}
+			 
+		  	 #Right {
+			 position:absolute;
+			 left: 70%;
+             top: 2.5%;
+			 width: 4%;
+			 cursor: pointer;
+		    	}
+		 
+		 
+		 	 #Debug {
+			 position:absolute;
+			 left: 10%;
+             top: 10%;
+			 cursor: pointer;
+		    	} 
+		 
+     </style>
+		 
+<body id="body" >
+	<div id="warning-message">
+    <img src="Elements/Rotate.png" width="100%" height="100%">
+</div>
+	
+<div id="home"> 
+		
+	<div id="Box" > <img src="Elements/Press/Background.png" width="100%" height="100%"></src></div>
+	
+	
+	
+	
+	<?php if ( $_GET['ProductsType'] == "Game" && $_GET['Page']==0) { ?>
+
+		<div id="SampleD" > 
+		<img src="Elements/Press/TV/0/0.png" width="100%" height="100%">
+	</div>	
+
+<div id="Sample_G_0" >
+
+<div id="79313855846"><script type="text/JavaScript" src="https://www.aparat.com/embed/7uJCR?data[rnddiv]=79313855846&data[responsive]=yes"></script></div>
+
+	</div>
+	
+	<?php } ?>
+	
+	<?php if ( $_GET['ProductsType'] == "Game" && $_GET['Page']==1) { ?>
+ 
+		<div id="SampleD" > 
+		<img src="Elements/Press/TV/1/1.png" width="100%" height="100%">
+	</div>	
+
+<div id="Sample_G_1" >
+<div id="35435041330"><script type="text/JavaScript" src="https://www.aparat.com/embed/NdZDC?data[rnddiv]=35435041330&data[responsive]=yes"></script></div>
+	</div>
+	
+	<?php } ?>
+	
+		<?php if ( $_GET['ProductsType'] == "Game" && $_GET['Page']==2) { ?>
+
+		<div id="SampleD" > 
+		<img src="Elements/Press/TV/2/2.png" width="100%" height="100%">
+	</div>	
+
+<div id="Sample_G_2" >
+<img src="Elements/Press/TV/2/2.jpg" width="70%" height="70%">
+	</div>
+	
+	<?php } ?>
+	
+		<?php if ( $_GET['ProductsType'] == "Game" && $_GET['Page']==3) { ?>
+
+		<div id="SampleD" > 
+		<img src="Elements/Press/TV/3/3.png" width="100%" height="100%">
+	</div>	
+	
+	<div id="Sample_G_3" >
+
+  <source src="Elements/Press/TV/3/3.jpg" type="video/mp4">
+
+	</div>
+	<?php } ?>
+	
+	
+	<?php if ( $_GET['ProductsType'] == "WebD" && $_GET['Page']==0) { ?>
+
+		<div id="SampleD" >
+			
+		<img src="Elements/Press/NewsAgency/0/0.png" width="100%" height="100%">
+		
+	</div>	
+
+<div id="Sample_W_0" >
+	<a href="https://www.farsnews.ir/news/13910213000843/بستری-برای-پیاده%E2%80%8Cسازی-ایده%E2%80%8Cهای-بازی%E2%80%8Cهای-رایانه%E2%80%8Cای-وجود-ندارد">
+<img src="Elements/Press/NewsAgency/0/00.png" width="100%" height="100%">
+			</a>
+	</div>
+	
+	<?php } ?>
+	
+	<?php if ( $_GET['ProductsType'] == "WebD" && $_GET['Page']==1) { ?>
+
+		<div id="SampleD" > 
+		<img src="Elements/Press/NewsAgency/1/1.png" width="100%" height="100%">
+	</div>	
+
+<div id="Sample_W_1" >
+<a href="https://www.imna.ir/news/274122/استعدادیابی-بازی-سازان-جوان-در-مسابقات-ملی-بازی-سازی-کاشان">
+ <img src="Elements/Press/NewsAgency/1/11.png" width="100%" height="100%">
+</a>
+	</div>
+	
+	<?php } ?>
+	
+		<?php if ( $_GET['ProductsType'] == "WebD" && $_GET['Page']==2) { ?>
+
+		<div id="SampleD" > 
+		<img src="Elements/Press/NewsAgency/2/2.png" width="100%" height="100%">
+	</div>	
+
+<div id="Sample_W_2" >
+  <img src="Elements/Press/NewsAgency/2/2.jpg" width="100%" height="100%">
+
+	</div>
+	
+	<?php } ?>
+	
+		<?php if ( $_GET['ProductsType'] == "WebD" && $_GET['Page']==3) { ?>
+
+		<div id="SampleD" > 
+		<img src="Elements/Products/WebDevelopment/3/3.png" width="100%" height="100%">
+	</div>	
+	
+<div id="Sample_W_3" >
+<img src="Elements/Products/WebDevelopment/3/3.jpg" width="100%" height="100%">
+	</div>
+	
+	<?php } ?>
+	
+
+	<?php if ( $_GET['ProductsType'] == "AppD" && $_GET['Page']==0) { ?>
+
+		<div id="SampleD" > 
+		<img src="Elements/Products/AppDevelopment/0/0.png" width="100%" height="100%">
+	</div>	
+
+<div id="Sample_A_0" >
+<video width="100%" height="100%" controls>
+  <source src="Elements/Products/AppDevelopment/0/0.mov" type="video/mp4" width="100%" height="100%">
+</video>
+	</div>
+	
+	<?php } ?>
+	
+	<?php if ( $_GET['ProductsType'] == "AppD" && $_GET['Page']==1) { ?>
+
+		<div id="SampleD" > 
+		<img src="Elements/Products/AppDevelopment/1/1.png" width="100%" height="100%">
+	</div>	
+
+<div id="Sample_A_1" >
+<video width="100%" height="100%" controls>
+  <source src="Elements/Products/AppDevelopment/1/1.mov" type="video/mp4" width="100%" height="100%">
+</video>
+	</div>
+	
+	<?php } ?>
+	
+		<?php if ( $_GET['ProductsType'] == "AppD" && $_GET['Page']==2) { ?>
+
+		<div id="SampleD" > 
+		<img src="Elements/Products/AppDevelopment/2/2.png" width="100%" height="100%">
+	</div>	
+
+<div id="Sample_A_2" >
+<img src="Elements/Products/AppDevelopment/2/2.jpg" width="100%" height="100%">
+	</div>
+	
+	<?php } ?>
+	
+		<?php if ( $_GET['ProductsType'] == "AppD" && $_GET['Page']==3) { ?>
+
+		<div id="SampleD" > 
+		<img src="Elements/Products/AppDevelopment/3/3.png" width="100%" height="100%">
+	</div>	
+	
+	<div id="Sample_A_3" >
+<img src="Elements/Products/AppDevelopment/3/3.jpg" width="100%" height="100%">
+	</div>
+	
+	<?php } ?>
+	
+	
+	
+	
+
+	
+	
+	
+	
+
+
+    <div id="Game" > <a  href="Press.php?ProductsType=Game&Page=0">
+		<img id="Game_B" src="Elements/Press/Buttons/Buttons_TV_1.png" width="100%" height="100%" onMouseOver="Game_2()" onMouseOut="Game_1()"></src> </a>
+	</div>
+	<script>
+function Game_2() {
+  document.getElementById("Game_B").src = "Elements/Press/Buttons/Buttons_TV_2.png";
+}
+		function Game_1() {
+  document.getElementById("Game_B").src = "Elements/Press/Buttons/Buttons_TV_1.png";
+}
+		
+		
+</script>
+
+    <div id="WebD" > <a href="Press.php?ProductsType=WebD&Page=0">  <img id="WebD_B" src="Elements/Press/Buttons/Buttons_NA_1.png" width="100%" height="100%" onMouseOver="WebD_2()" onMouseOut="WebD_1()"></src> </a></div>
+	
+<script>
+		
+function WebD_2() {
+  document.getElementById("WebD_B").src = "Elements/Press/Buttons/Buttons_NA_2.png";
+}
+		function WebD_1() {
+  document.getElementById("WebD_B").src = "Elements/Press/Buttons/Buttons_NA_1.png";
+}
+		
+		
+</script> <!--
+    <div id="AppD" > <a href="Press.php?ProductsType=AppD&Page=0"> <img id="AppD_B" src="Elements/Press/Buttons/Buttons_NM_1.png" width="100%" height="100%" onMouseOver="AppD_2()" onMouseOut="AppD_1()"></src> </a></div>
+	<script>
+function AppD_2() {
+  document.getElementById("AppD_B").src = "Elements/Press/Buttons/Buttons_NM_2.png";
+}
+		function AppD_1() {
+  document.getElementById("AppD_B").src = "Elements/Press/Buttons/Buttons_NM_1.png";
+}
+		!-->
+		
+</script>
+ 
+<div id="Back" > <a href="index.html"> <img id="Back_B" src="Elements/Products/Buttons/Buttons_Back_1.png"  width="100%" height="100%" onMouseOver="Back_2()" onMouseOut="Back_1()"></src> </a></div>
+	
+<script>
+function Back_2() {
+  document.getElementById("Back_B").src = "Elements/Products/Buttons/Buttons_Back_2.png";
+}
+		function Back_1() {
+  document.getElementById("Back_B").src = "Elements/Products/Buttons/Buttons_Back_1.png";
+}
+	</script>
+
+<?php
+if ( $_GET['Page'] == "3" )
+{
+    $intPageP = "0";
+}
+else
+	{
+    $intPageP = $_GET['Page']+1;
+}
+
+
+
+if ( $_GET['Page'] == "0" )
+{
+	$intPageM = "3";
+}
+else
+{
+	$intPageM = $_GET['Page']-1;
+}
+?>
+
+<div id="Left" > <a href= "Press.php?ProductsType=<?php echo $_GET['ProductsType']; ?>&Page=<?php echo $intPageM;?>" > <img id="Left_B" src="Elements/Products/Buttons/Buttons_Left_1.png" width="100%" height="100%" onMouseOver="Left_2()" onMouseOut="Left_1()"></src> </a> </div>
+	
+<script>
+	
+function Left_2() {
+  document.getElementById("Left_B").src = "Elements/Products/Buttons/Buttons_Left_2.png";
+}
+		function Left_1() {
+  document.getElementById("Left_B").src = "Elements/Products/Buttons/Buttons_Left_1.png";
+}
+	
+	
+	</script>
+
+
+<div id="Right" > <a href= "Press.php?ProductsType=<?php echo $_GET['ProductsType']; ?>&Page=<?php echo $intPageP;?>" > <img id="Right_B" src="Elements/Products/Buttons/Buttons_Right_1.png" width="100%" height="100%" onMouseOver="Right_2()" onMouseOut="Right_1()"></src> </a></div>
+	
+<script>
+function Right_2() {
+  document.getElementById("Right_B").src = "Elements/Products/Buttons/Buttons_Right_2.png";
+}
+		function Right_1() {
+  document.getElementById("Right_B").src = "Elements/Products/Buttons/Buttons_Right_1.png";
+}
+	</script>
+	
+
+    
+    <!--    
+   
+
+    <div id="Top5" > <img src="Elements/Products/Top_5.png" width="100%" height="100%"></src></div>
+    <div id="Top4" > <img src="Elements/Products/Top_4.png" width="100%" height="100%"></src></div>
+    
+	<div id="Top2" > <img src="Elements/Products/Top_2.png" width="100%" height="100%"></src></div>
+    <div id="Top3" > <img src="Elements/Products/Top_3.png" width="100%" height="100%"></src></div>
+    <div id="Top1" > <img src="Elements/Products/Top_1.png" width="100%" height="100%"></src></div>
+!-->
+
+</div>
+
+<!--
+<div id="Debug" > Variable <?php echo $_GET['ProductsType']; ?> </div>
+-->
+
+  
+   
+</body>
+</html>
